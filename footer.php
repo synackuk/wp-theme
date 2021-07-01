a0991bc7ad5496d69334ae4c07381c904f54b04d
synackuk <53281989+synackuk@users.noreply.github.com>
2021-07-01 13:59:13 +0100

    Add files via upload

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 <nav>
 <span><h2><a href="https://github.com/synackuk" target="_blank"><i class="fa github"></i></a></h2></span>
 <span><h2><a href="https://twitter.com/synackuk" target="_blank"><i class="fa twitter"></i></a></h2></span>
+<span><h2><a href="https://www.patreon.com/synackuk" target="_blank"><i class="fa patreon"></i></a></h2></span>
 </nav>
 </footer>
 </body>
